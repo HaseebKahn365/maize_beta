2607b1db25227e8a79407b573acf309cdc7ab18d
Abdul Haseeb <58126830+HaseebKahn365@users.noreply.github.com>
2024-04-05 21:39:38 +0500

    Update player.dart

--- lib/player.dart
+++ lib/player.dart
@@ -8,13 +8,12 @@ import 'package:sensors_plus/sensors_plus.dart';
 
 //the player should be able to readjust its initaial position when the user holds the screen and moves the device. this will give the user a better and comfortable experience when playing the game allowing the user to take a break.
 
-//lets enable touch events on the player component
 class Player extends PositionComponent with HasGameRef<MyGame>, TapCallbacks {
   Color _color = Colors.white;
   final _playerPaint = Paint();
 
   Player({
-    this.playerRadius = 12,
+    this.playerRadius = 35,
   }) : super(
           position: Vector2.all(150),
           priority: 20,
@@ -41,25 +40,28 @@ class Player extends PositionComponent with HasGameRef<MyGame>, TapCallbacks {
     super.onMount();
   }
 
+  bool cond = false;
+
+  @override
+  void onLongTapDown(TapDownEvent event) {
+    // Set cond to true to start updating newPosition
+    cond = !cond;
+    super.onLongTapDown(event);
+  }
+
   late Vector2 initialPosition;
   var newPosition = Vector2.zero();
 
   @override
   void update(double dt) {
-    //initial position of the player is 150,150
-    //the displacement vector from the intial point will be used as the velocity of the player
+    if (cond) {
+      newPosition = initialPosition;
+    }
+
     newPosition += _velocity * dt * 1500;
     position += (newPosition - initialPosition) * dt;
 
     super.update(dt);
-  }
-
-  @override
-  void onLongTapDown(TapDownEvent event) {
-    //reset the initial position of the player to the current position of gyroscope sensor
-    initialPosition = _velocity;
-
-    super.onLongTapDown(event);
   }
 
   @override
@@ -76,7 +78,7 @@ class Player extends PositionComponent with HasGameRef<MyGame>, TapCallbacks {
     final text = 'x: ${_velocity.x.toStringAsFixed(2)}\ny: ${_velocity.y.toStringAsFixed(2)}';
     final textStyle = TextStyle(
       color: Colors.black,
-      fontSize: 10,
+      fontSize: 20,
     );
     final textSpan = TextSpan(
       text: text,
