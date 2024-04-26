df81d04e34c8b5f4582f94057b57b7cf343f8c05
Abdul Haseeb <58126830+HaseebKahn365@users.noreply.github.com>
2024-04-26 23:52:17 +0500

    fixed the diamonds

--- lib/Components/Collectables/Diamond.dart
+++ lib/Components/Collectables/Diamond.dart
@@ -5,8 +5,8 @@ class Diamond extends Collectable {
   Diamond({position, size, int score = 300}) : super(position: position, size: size, icon: Icons.diamond_outlined, color: Colors.white, score: 300);
 
   @override
-  void _affectScore() {
-    game.diamonds.value += 1;
+  void affectScore() {
     super.game.incrementScore(300);
+    game.diamonds.value += 1;
   }
 }
